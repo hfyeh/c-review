b7d32c7b0335f57a53341fba85be3600e5a4d9d6
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2023-05-05 11:13:24 +0800

    Add merge_sort

--- linked_list_quiz.c
+++ linked_list_quiz.c
@@ -46,6 +46,89 @@ void FuncC(struct node **start, int value1, int value2) {
     next->prev = new_node;
 }
 
+
+void separate(struct node *start, struct node **left, struct node **right) {
+    struct node *slow = start, *fast = start->next;
+    if (fast == start) {
+        *left = start;
+        *right = NULL;
+        return;
+    }
+    while (fast != start && fast->next != start) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    *left = start;
+    *right = slow->next;
+
+    // Deal with *right
+    (*right)->prev = start->prev;
+    start->prev->next = *right;
+
+    // Deal with *left
+    start->prev = slow;
+    slow->next = start;
+}
+
+struct node *pop_first_node(struct node **start) {
+    if (!*start)
+        return NULL;
+    struct node *last = (*start)->prev;
+    struct node *first = *start;
+    if (first == last) {
+        *start = NULL;
+        return first;
+    }
+    *start = first->next;
+    (*start)->prev = last;
+    last->next = *start;
+    return first;
+}
+
+void append_node(struct node **start, struct node *new_node) {
+    if (!*start) {
+        *start = new_node;
+        new_node->prev = new_node->next = new_node;
+        return;
+    }
+    struct node *last = (*start)->prev;
+    new_node->next = *start;
+    (*start)->prev = new_node;
+    new_node->prev = last;
+    last->next = new_node;
+}
+
+struct node *merge_linked_list(struct node *left, struct node *right) {
+    struct node *result = NULL;
+    while (left != NULL && right != NULL) {
+        struct node *temp;
+        if (left->data <= right->data) {
+            temp = pop_first_node(&left);
+        } else {
+            temp = pop_first_node(&right);
+        }
+        append_node(&result, temp);
+    }
+
+    struct node *remainder = left ? left : right;
+    while (remainder) {
+        struct node *temp = pop_first_node(&remainder);
+        append_node(&result, temp);
+    }
+
+    return result;
+}
+
+void merge_sort(struct node **start) {
+    if (!*start || (*start)->next == *start)
+        return;
+    struct node *left, *right;
+    separate(*start, &left, &right);
+    merge_sort(&left);
+    merge_sort(&right);
+    *start = merge_linked_list(left, right);
+}
+
 void display(struct node *start) {
     struct node *temp = start;
     printf("Traversal in forward direction \n");
@@ -67,6 +150,7 @@ int main() {
     FuncA(&start, 72);
     FuncA(&start, 86);
     FuncC(&start, 63, 51);
+    merge_sort(&start);
     display(start);
     return 0;
 }
