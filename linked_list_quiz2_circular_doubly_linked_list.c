2bfab840109070df1bac7839fb3a1030fe2a9f0e
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2023-05-06 18:10:56 +0800

    Make linked list doubly linked

--- linked_list_quiz2_circular_doubly_linked_list.c
+++ linked_list_quiz2_circular_doubly_linked_list.c
@@ -4,69 +4,73 @@
 typedef struct __list {
     int data;
     struct __list *next;
+    struct __list *prev;
 } list;
+
+list *pop_first_node(list **start) {
+    if (!*start)
+        return NULL;
+    list *first = *start;
+    if (first->next == first) {
+        *start = NULL;
+        return first;
+    }
+    list *last = first->prev;
+    *start = first->next;
+    first->next->prev = last;
+    last->next = first->next;
+    return first;
+}
+
+void append_node(list **start, list *new_node) {
+    if (!*start) {
+        *start = new_node;
+        new_node->prev = new_node->next = new_node;
+        return;
+    }
+    list *last = (*start)->prev;
+    new_node->next = *start;
+    (*start)->prev = new_node;
+    new_node->prev = last;
+    last->next = new_node;
+}
 
 list *sort(list *start) {
     if (!start || start->next == start)
         return start;
     list *left = start;
     list *right = left->next;
-
+    right->prev = start->prev;
+    start->prev->next = right;
     left->next = start;
-    list *current = right;
-    while (current->next != start)
-        current = current->next;
-    current->next = right;
+    left->prev = start;
 
     left = sort(left);
     right = sort(right);
 
-    current = left;
-    while (current->next != left)
-        current = current->next;
-    current->next = NULL;
-
-    current = right;
-    while (current->next != right)
-        current = current->next;
-    current->next = NULL;
-
-    for (list *merge = NULL; left || right;) {
-        if (!right || (left && left->data < right->data)) {
-            if (!merge) {
-                start = merge = left;
-            } else {
-                merge->next = left;
-                merge = merge->next;
-            }
-            left = left->next;
+    list *result = NULL;
+    while (left && right) {
+        list *node;
+        if (left->data < right->data) {
+            node = pop_first_node(&left);
         } else {
-            if (!merge) {
-                start = merge = right;
-            } else {
-                merge->next = right;
-                merge = merge->next;
-            }
-            right = right->next;
+            node = pop_first_node(&right);
         }
-        merge->next = start;
+        append_node(&result, node);
     }
-    return start;
-}
 
-void append_node(list **start, int data) {
-    list *new_node = malloc(sizeof(list));
-    new_node->data = data;
-    if (!*start) {
-        *start = new_node;
-        new_node->next = new_node;
-        return;
+    list *remaining = left ? left : right;
+
+    if (remaining) {
+        list *last = remaining->prev;
+        result->prev->next = remaining;
+        remaining->prev = result->prev;
+
+        result->prev = last;
+        result->prev->next = result;
     }
-    list *current = *start;
-    while (current->next != *start)
-        current = current->next;
-    current->next = new_node;
-    new_node->next = *start;
+
+    return result;
 }
 
 void print_list(list *start) {
@@ -80,8 +84,11 @@ int main() {
     const int MAX = 10;
 
     list *start = NULL;
-    for (int i = 0; i < MAX; i++)
-        append_node(&start, MAX - i);
+    for (int i = 0; i < MAX; i++) {
+        list *new_node = malloc(sizeof(list));
+        new_node->data = MAX - i;
+        append_node(&start, new_node);
+    }
 
     print_list(start);
 
