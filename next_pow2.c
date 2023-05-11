9065b4da77611165f19a1c8679543a7ebd6a8fb5
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2023-05-11 16:17:32 +0800

    Implement next_pow2 by using __builtin_clzl

--- next_pow2.c
+++ next_pow2.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <inttypes.h>
+#include <stdio.h>
 
 uint64_t next_pow2(uint64_t x) {
     x |= x >> 1;
@@ -15,9 +15,16 @@ uint64_t next_pow2(uint64_t x) {
     return x + 1;
 }
 
+uint64_t next_pow2_by_builtin_clzl(uint64_t x) {
+    if (!x)
+        return 1;
+    return 1UL << (64 - __builtin_clzl(x));
+}
+
 int main() {
-    uint64_t x = 7;
+    uint64_t x = 8;
     printf("x = 0x%016" PRIx64 ", Decimal = %lu\n", x, x);
     printf("next_pow2(x) = 0x%016" PRIx64 ", Decimal = %lu\n", next_pow2(x), next_pow2(x));
+    printf("next_pow2_by_builtin_clzl(x) = 0x%016" PRIx64 ", Decimal = %lu\n", next_pow2_by_builtin_clzl(x), next_pow2_by_builtin_clzl(x));
     return 0;
 }
